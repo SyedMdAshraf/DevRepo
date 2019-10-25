d84fc410d2d550c41766386ee5226e8aee77786c
SyedMdAshraf <syed-md.ashraf@capgemini.com>
2019-10-25 12:05:11 +0530

    First Comment

---
+++ DevOps.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class DevOps
+    {
+        int a = 7;
+        int b = 5;
+        public DevOps()
+        {
+            //Some more Changes
+        }
+        public DevOps(int x,int y)
+        {
+            int a = x;
+            int b= y;
+        }
+    }
+}
